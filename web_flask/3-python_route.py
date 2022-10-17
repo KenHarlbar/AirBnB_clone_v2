e739df702a4ca6f23bd13a311c244e57e30de368
baaioy <balotech514@gmail.com>
2022-10-17 03:03:59 +0100

    Start a new flask web app

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" A script that starts a Flask web application """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Function called with / route """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Function called with /hbnb route """
+    return 'HBNB'
+
+
+@app.route('/c/<string:text>', strict_slashes=False)
+def c_text(text):
+    """ Function called with /c/ route """
+    return 'C %s' % text.replace('_', ' ')
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text='is cool'):
+    """ Function called with /python/ route """
+    if text is not 'is cool':
+        text = text.replace('_', ' ')
+    return 'Python %s' % text
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
